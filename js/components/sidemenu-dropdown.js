94b6690eed4a49356081b69abbfcaa717ef9f956
Itse Sagay <codeplay.erniie@gmail.com>
2022-08-20 12:54:41 +0100

    Refactored side-menu

--- js/components/sidemenu-dropdown.js
+++ js/components/sidemenu-dropdown.js
@@ -2,7 +2,7 @@ export default function sideMenuDropDown() {
     let sideNav = document.querySelector('.side__nav');
 
     if (sideNav) {
-        let menuItems = sideNav.querySelectorAll('.menu__item');
+        let menuItems = sideNav.querySelectorAll('.menu__item--has-submenu');
         
         menuItems.forEach((item) => {
             item.addEventListener('click', function (e) {
